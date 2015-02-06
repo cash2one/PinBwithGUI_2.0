af10d223326cf193e61e5d4569b35203685e0125
xuhuan <xuhuanxxx@gmail.com>
2015-02-06 20:13:36 +0800

    str

--- q1.py
+++ q1.py
@@ -1,5 +1,12 @@
 __author__ = 'xuhuan'
-name_lengths = map(len, ["Mary", "Isla", "Sam"])
-s = lambda x, y: x * y
-print(list(name_lengths))
-print(s(1, 5))
+import csv
+games = ['三国志', '三国群英传', '三国无双', '三国无敌', '三国战记', '三国争霸', '三国风云', '三国英杰传', '三国游戏', '三国群侠传', '三国英雄', '最三国', '三国赵云传']
+ios = ['ios', 'ios版', 'iphone', '苹果', 'app', 'ipad', 'ipad版']
+apk = ['apk', '安卓', '安卓版', '手机游戏', '手游', '手机版']
+
+dct = {}
+for j in [ios, apk]:
+    for i in games:
+        lst = list(map(lambda x, y=i: y + x, j))
+        dict1 = dict(zip(games[]))
+print(ls1)
